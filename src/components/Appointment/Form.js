3ed76c3897388940f7382934c67edda6edaee1fe
Adam-Marx <adamalexmarx@gmail.com>
2023-02-23 01:25:21 -0800

    Import Appointment to Application.js to display static scheduler page

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,52 @@
-import React from "react";
+import InterviewerList from "components/InterviewerList.js"
+import Button from "components/Button.js"
+import React, { useState } from 'react';
 
 export default function Form(props) {
+  //State tracking: student string, interviewer id
+  //Actions: setStudent, setInterviewer
+  //props: student, interviewers, interviewer, onSave, onCancel
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const onCancel = props.onCancel
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
 
   return (
-    <p></p>
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList 
+            interviewers={props.interviewers}
+            value={interviewer}
+            onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
   )
 
 }
