22bd62aac00f3b19785d5550c963e0445c013ae7
Adam-Marx <adamalexmarx@gmail.com>
2023-02-28 10:54:47 -0800

    update spots

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,31 +4,18 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay  } from "../helpers/selectors.js"
+import useApplicationData from "../hooks/useApplicationData.js"
 
 
 export default function Application(props) {
-
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {}
-  });
-
-  
-
-  const setDay = day => setState({ ...state, day });
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then(all => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
-  }, []);
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+    updateInterview
+  } = useApplicationData();
 
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay (state, state.day);
@@ -43,6 +30,9 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+        updateInterview={updateInterview}
       />
     )
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,38 +4,154 @@ import Header from "components/Appointment/Header.js"
 import Empty from "components/Appointment/Empty.js"
 import Show from "components/Appointment/Show.js"
 import Form from "components/Appointment/Form.js"
+import Confirm from "components/Appointment/Confirm.js"
+import Status from "components/Appointment/Status.js"
+import Error from "components/Appointment/Error.js"
 import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
 
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
-  // const FORM = "FORM";
-  // const DELETING = "DELETING";
-  // const CONFIRM = "CONFIRM";
-  // const EDIT = "EDIT";
-  // const ERROR_SAVE = "ERROR_SAVE";
-  // const ERROR_DELETE = "ERROR_DELETE";
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+  const SAVING = 'SAVING';
+  const DELETING = 'DELETING';
+  const CONFIRM = 'CONFIRM'
+  const EDIT = 'EDIT';
+  const ERROR_SAVE = 'ERROR_SAVE';
+  const ERROR_DELETE = 'ERROR_DELETE';
 
   const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
+      props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    transition(SAVING);
+    
+    props
+      .bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(() => {
+        transition(ERROR_SAVE, true)
+      });
+  };
+
+  function deleteAppointment() {
+    transition(CONFIRM);
+  }
+
+  function editAppointment(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    transition(EDIT);
+  
+    props
+      .updateInterview(props.id, interview)
+      .then(() => transition(SHOW))
+  }
+
+  
+  function handleEditAppointment(name, interviewer) {
+    editAppointment(name, interviewer);
+  }
+    
+  function onConfirmDelete() {
+    transition(DELETING, true);
+
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(() => {
+        transition(ERROR_DELETE, true)
+      });
+  }
+  
+  
 
   return (
     <article className="appointment">
+
         <Header
           time={props.time}
+        />
+
+        {mode === EMPTY && (
+          <Empty 
+            onAdd={() => transition(CREATE)} 
           />
-         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-         {mode === CREATE && <Form interviewers={[]} onCancel={back}/>}
-         {mode === SHOW && (
-            <Show
-              student={props.interview.student}
-              interviewer={props.interview.interviewer}
-            />
-        )}
+          )
+        }
+
+        {mode === CREATE && (
+          <Form 
+            interviewers={props.interviewers}
+            onCancel={back} onSave={save}
+          />
+          )
+        }
+
+        {mode === EDIT && (
+          <Form
+            student={props.interview.student}
+            interviewer={props.interview.interviewer.id}
+            interviewers={props.interviewers}
+            onSave={save}
+            onCancel={back}
+          />
+          )
+        }
+
+        {props.interview && mode === SHOW && (
+          <Show
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+            onDelete={() => deleteAppointment(props.id)}
+            onEdit={() => handleEditAppointment(props.interview.student, props.interview.interviewer)}
+          />
+         )
+        }
+        
+        {mode === SAVING && <Status message={'Saving appointment...'}/>}
+
+        {mode === DELETING && (
+          <Status 
+          message={'Deleting appointment...'}
+          />
+          )
+        } 
+
+        {mode === CONFIRM && (
+        <Confirm
+          onCancel={() => back()}
+          message={'Are you sure you want to delete this appointment?'}
+          onConfirm={onConfirmDelete}
+        />
+        )
+      }
+
+      {mode === ERROR_SAVE && (
+        <Error
+          message={'There was an error while trying to save your appointment.'}
+          onClose={back}
+        />
+        )
+      }
+
+      {mode === ERROR_DELETE && (
+        <Error
+          message={'There was an error while trying to delete your appointment.'}
+          onClose={back}
+        />
+        )
+      }
+      
     </article>
   )
 };
